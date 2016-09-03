f0f95f21e7f27feec009acf2333de7805a221c7f
Eric Meyers <eric.meyers12@gmail.com>
2016-09-03 17:02:32 -0500

    Added some library files and started interrupt handling

---
+++ src/interrupts.h
@@ -0,0 +1,24 @@
+/*
+ * interrupts.h
+ *
+ *  Created on: Sep 3, 2016
+ *      Author: Eric_Meyers
+ */
+
+#ifndef _INTERRUPTS_H_
+#define _INTERRUPTS_H_
+
+///* Keyboard interrupt asm wrapper */
+//extern void keyboard_handler();
+//
+///* Clock interrupt asm wrapper */
+//extern void rtc_handler();
+//
+///* PIT interrupt asm wrapper */
+//extern void pit_handler();
+//
+///* System Call asm wrapper */
+//extern void system_call_handler();
+
+
+#endif /* SRC_INTERRUPTS_H_ */
