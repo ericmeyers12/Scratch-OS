65ab2381ec3342fad994624b2a81d35603c34230
Eric Meyers <eric.meyers12@gmail.com>
2016-09-05 19:47:31 -0500

    Things still broke, but making progress on multiboot/linker problem

--- src/boot.s
+++ src/boot.s
@@ -5,13 +5,6 @@
 #include "multiboot.h"
 #include "x86_desc.h"
 
-# Declare constants for the multiboot header.
-.set ALIGN,    1<<0             # align loaded modules on page boundaries
-.set MEMINFO,  1<<1             # provide memory map
-.set FLAGS,    ALIGN | MEMINFO  # this is the Multiboot 'flag' field
-.set MAGIC,    0x1BADB002       # 'magic number' lets bootloader find the header
-.set CHECKSUM, -(MAGIC + FLAGS) # checksum of above, to prove we are multiboot
-
 # Declare a multiboot header that marks the program as a kernel. These are magic
 # values that are documented in the multiboot standard. The bootloader will
 # search for this signature in the first 8 KiB of the kernel file, aligned at a
@@ -19,16 +12,17 @@
 # forced to be within the first 8 KiB of the kernel file.
 .section .multiboot
 .align 4
-.long MAGIC
-.long FLAGS
-.long CHECKSUM
+.long MULTIBOOT_HEADER_MAGIC
+.long MULTIBOOT_HEADER_FLAGS
+.long -(MULTIBOOT_HEADER_MAGIC+MULTIBOOT_HEADER_FLAGS)
 
-# Set up stack to be 16kB large and define boundaries.
-.section .bss
-.align 16
-stack_bottom:
-.skip 16384  #16 kB
-stack_top:
+
+; # Set up stack to be 16kB large and define boundaries.
+; .section .bss
+; .align 16
+; stack_bottom:
+; .skip 4194304  # 4MB
+; stack_top:
 
 # Linker will jump to this section as its entry (as defined in linker.ld)
 .section .text
@@ -58,8 +52,12 @@ continue:
 	pop %edx
 	pop %eax
 
+	# Load CS with the new descriptor value
+	ljmp    $KERNEL_CS, $keep_going
+
+keep_going:
 	# Move ESP to top of stack (grows downwards on x86 systems).
-	mov $stack_top, %esp
+	mov $0x800000, %esp
 
 	# Set up the rest of the segment selector registers
 	movw    $KERNEL_DS, %cx
@@ -82,4 +80,3 @@ continue:
 	cli
 1:	hlt
 	jmp 1b
-
